d5ac23087bed49f711a63d26412c29fffacae2dd
chaoocharles <charleykairu@gmail.com>
2021-03-25 16:38:15 +0300

    slideshow at hero section

--- src/components/layout/HeroSection.jsx
+++ src/components/layout/HeroSection.jsx
@@ -1,11 +1,74 @@
 import React from "react";
 import "./HeroSection.css";
+import {
+  MDBCarousel,
+  MDBCarouselCaption,
+  MDBCarouselInner,
+  MDBCarouselItem,
+  MDBView,
+  MDBMask,
+  MDBContainer,
+} from "mdbreact";
+
+import "bootstrap-css-only/css/bootstrap.min.css";
+// import "mdbreact/dist/css/mdb.css";
+
+import pot8 from "../../assets/images/products/pot-and-pans-set-8.png";
+import pot3 from "../../assets/images/products/pot-and-pans-set-3.jpg";
+import cups4 from "../../assets/images/products/cups-4.jpg";
 
 const HeroSection = () => {
   return (
     <>
       <div className="hero-section">
-        <h2>Kayz Households</h2>
+        <div className="hero-text">
+          <h2 className="new-arrivals">New Arrivals</h2>
+          <h2 className="special-offer">Special Offer</h2>
+          <button>Shop Now</button>
+        </div>
+        <div className="hero-slider">
+          <MDBContainer>
+            <MDBCarousel
+              activeItem={1}
+              length={3}
+              showControls={false}
+              showIndicators={false}
+              className="z-depth-1"
+              slide
+            >
+              <MDBCarouselInner>
+                <MDBCarouselItem itemId="1">
+                  <MDBView>
+                    <img className="d-block" src={pot8} alt="First slide" />
+                    {/* <MDBMask overlay="black-light" /> */}
+                  </MDBView>
+                  <MDBCarouselCaption>
+                    {/* <h3 className="h3-responsive">40% Off</h3> */}
+                    {/* <p>First text</p> */}
+                  </MDBCarouselCaption>
+                </MDBCarouselItem>
+                <MDBCarouselItem itemId="2">
+                  <MDBView>
+                    <img className="d-block" src={pot3} alt="Second slide" />
+                    {/* <MDBMask overlay="black-light" /> */}
+                  </MDBView>
+                  <MDBCarouselCaption>
+                    {/* <h3 className="h3-responsive">Buy Now</h3> */}
+                  </MDBCarouselCaption>
+                </MDBCarouselItem>
+                <MDBCarouselItem itemId="3">
+                  <MDBView>
+                    <img className="d-block" src={cups4} alt="Third slide" />
+                    {/* <MDBMask overlay="black-light" /> */}
+                  </MDBView>
+                  <MDBCarouselCaption>
+                    {/* <h3 className="h3-responsive">Limited Sales</h3> */}
+                  </MDBCarouselCaption>
+                </MDBCarouselItem>
+              </MDBCarouselInner>
+            </MDBCarousel>
+          </MDBContainer>
+        </div>
       </div>
     </>
   );
