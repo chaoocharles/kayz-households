04e33e481969ba189934eb919899fe8c72b1d557
chaoocharles <charleykairu@gmail.com>
2021-04-27 14:40:46 +0300

    fixed issues

--- src/App.js
+++ src/App.js
@@ -11,8 +11,9 @@ import "react-toastify/dist/ReactToastify.css";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Checkout from "./components/cart/Checkout";
-import AdminDashboard from "./components/admin/AdminDashboard"
+import AdminDashboard from "./components/admin/AdminDashboard";
 import Orders from "./components/orders/Orders";
+import ProductDetails from "./components/products/ProductDetails";
 
 function App() {
   return (
@@ -20,15 +21,18 @@ function App() {
       <BrowserRouter>
         <ToastContainer />
         <NavBar />
-        <Switch>
-          <Route path="/admin-dashboard" component={AdminDashboard} />
-          <Route path="/orders" component={Orders} />
-          <Route path="/login" component={Login} />
-          <Route path="/register" component={Register} />
-          <Route path="/cart/checkout" component={Checkout} />
-          <Route path="/cart" component={CartContainer} />
-          <Route path="/" component={Home} />
-        </Switch>
+        <div className="content-container">
+          <Switch>
+            <Route path="/product/:id" component={ProductDetails} />
+            <Route path="/admin-dashboard" component={AdminDashboard} />
+            <Route path="/orders" component={Orders} />
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+            <Route path="/cart/checkout" component={Checkout} />
+            <Route path="/cart" component={CartContainer} />
+            <Route path="/" component={Home} />
+          </Switch>
+        </div>
         <Footer />
       </BrowserRouter>
     </div>
---
+++ src/components/products/ProductDetails.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProductDetails = () => {
+  return (
+    <>
+      <h2>Product Details</h2>
+    </>
+  );
+};
+
+export default ProductDetails;
